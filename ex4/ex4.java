0e829a54ec8b9ecc005cdc2ec241f154cb38d754
Kielbo <kielbo@pop-os.localdomain>
2022-01-05 18:02:34 +0100

    added the method lenght

--- ex4/ex4.java
+++ ex4/ex4.java
@@ -2,16 +2,16 @@ public class ex4
 {
     public static void windowPosSum(int[] a, int n)
     {
-        for(int i = 0; i < 6; i++)
+        for(int i = 0; i < a.length; i++)
         {
             if(a[i] < 0)        //se è negativo salta un ciclo
                 continue;
-            if(i == 5)          //se è arrivato all'ultimo 
-                break;          //ferma il ciclo
+            if(i == a.length() - 1)  //se è arrivato all'ultimo 
+                break;             //ferma il ciclo
 
             for(int j = 1; j <= n; j++)
             {
-                if( (i + j) > 5)    //se finiscono i numeri da 
+                if( (i + j) > a.length - 1)    //se finiscono i numeri da 
                 {                   //sommare salta un ciclo
                     continue;
                 }
