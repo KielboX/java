d6484553debf95cfafc4f9e4b84a8c8de769e8b0
Kielbo <kielbo@pop-os.localdomain>
2022-01-05 17:49:49 +0100

    added ex4

---
+++ ex4/ex4.java
@@ -0,0 +1,30 @@
+public class ex4 
+{
+    public static void windowPosSum(int[] a, int n)
+    {
+        for(int i = 0; i < 6; i++)
+        {
+            if(a[i] < 0)        //se è negativo salta un ciclo
+                continue;
+            if(i == 5)          //se è arrivato all'ultimo 
+                break;          //ferma il ciclo
+
+            for(int j = 1; j <= n; j++)
+            {
+                if( (i + j) > 5)    //se finiscono i numeri da 
+                {                   //sommare salta un ciclo
+                    continue;
+                }
+                a[i] = a[i] + a[i + j];
+            }
+        }
+    }
+    public static void main(String[] args) 
+    {
+        int[] a = {1, 2, -3, 4, 5, 4};
+        int n = 3;
+        windowPosSum(a, n);
+
+        System.out.println(java.util.Arrays.toString(a));
+    }
+}
